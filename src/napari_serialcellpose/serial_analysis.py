6664d126c8077e6714ab3fedb12c50a2ce59f4f4
guiwitz <guiwitz@users.noreply.github.com>
2022-04-28 23:08:15 +0200

    display and export properties

--- src/napari_serialcellpose/serial_analysis.py
+++ src/napari_serialcellpose/serial_analysis.py
@@ -1,8 +1,11 @@
 from pathlib import Path
 import skimage.io
+import skimage.segmentation
+from skimage.measure import regionprops_table
+import pandas as pd
 
 def run_cellpose(image_path, cellpose_model, output_path, scaling_factor=1,
-                 diameter=None):
+                 diameter=None, clear_border=True):
     """Run cellpose on image"""
 
 
@@ -23,12 +26,72 @@ def run_cellpose(image_path, cellpose_model, output_path, scaling_factor=1,
     # run cellpose
     cellpose_output = cellpose_model.eval(image, channels = [[0,0]])
     cellpose_output = cellpose_output[0]
+
+    if clear_border is True:
+
+        cellpose_output = [skimage.segmentation.clear_border(im) for im in cellpose_output]
+        cellpose_output = [skimage.segmentation.relabel_sequential(im)[0] for im in cellpose_output]
     
     # save output
     for im, p in zip(cellpose_output, image_path):
         save_path = output_path.joinpath(p.stem+'_mask.tif')
         skimage.io.imsave(save_path, im, check_contrast=False)
 
+        compute_props(
+            label_image=im,
+            output_path=output_path,
+            image_name=p
+            )
+
     return cellpose_output
 
+
+def compute_props(label_image, output_path, image_name):
+    """Compute cell properties"""
+    
+    image_name = Path(image_name)
+    output_path = Path(output_path).joinpath('tables')
+    if not output_path.exists():
+        output_path.mkdir(parents=True)
+
+    props = regionprops_table(label_image,
+                          properties=('label', 'area', 'eccentricity', 'solidity', 'feret_diameter_max'))
+        
+    props['eccentricity'] = props['eccentricity'].round(2)
+    props['feret_diameter_max'] = props['feret_diameter_max'].round(2)
+    props['solidity'] = props['solidity'].round(2)
+
+    props = pd.DataFrame(props)
+    props.to_csv(output_path.joinpath(image_name.stem+'_props.csv'), index=False)
+
+
+def load_props(output_path, image_name):
+    """Load properties from file"""
+
+    # get file name
+    image_name = Path(image_name)
+    output_path = Path(output_path).joinpath('tables')
+
+    # load properties
+    props_path = Path(output_path).joinpath(image_name.stem+'_props.csv')
+    if props_path.exists():
+        props = pd.read_csv(props_path)
+
+    return props
+
+def load_allprops(output_path):
+    """Load properties from file"""
+
+    # get file name
+    output_path = Path(output_path).joinpath('tables')
+    table_names = list(output_path.glob('*_props.csv'))
+    
+    all_props = []
+    for p in table_names:
+        props = pd.read_csv(p)
+        props['name'] = p.stem
+        all_props.append(props)
+    all_props = pd.concat(all_props)
+
+    return all_props
 
--- src/napari_serialcellpose/serial_widget.py
+++ src/napari_serialcellpose/serial_widget.py
@@ -1,11 +1,11 @@
 from qtpy.QtWidgets import (QWidget, QVBoxLayout,QFileDialog, QPushButton,
-QSpinBox, QLabel, QGridLayout, QHBoxLayout, QGroupBox, QComboBox)
+QSpinBox, QLabel, QGridLayout, QHBoxLayout, QGroupBox, QComboBox, QTabWidget)
 from .folder_list_widget import FolderList
-from .serial_analysis import run_cellpose
+from .serial_analysis import run_cellpose, load_props, load_allprops
 
 from pathlib import Path
 import skimage.io
-from skimage.measure import regionprops_table
+import numpy as np
 from cellpose import models
 from napari_skimage_regionprops._table import TableWidget
 
@@ -15,20 +15,11 @@ from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg, NavigationTool
 from matplotlib.figure import Figure
 
 
-class MplCanvas(FigureCanvasQTAgg):
-
-    def __init__(self, parent=None, width=5, height=4, dpi=100):
-        fig = Figure(figsize=(width, height), dpi=dpi)
-        self.axes = fig.add_subplot(111)
-        super(MplCanvas, self).__init__(fig)
-
-
-
 class SerialWidget(QWidget):
-    # your QWidget.__init__ can optionally request the napari viewer instance
-    # in one of two ways:
-    # 1. use a parameter called `napari_viewer`, as done here
-    # 2. use a type annotation of 'napari.viewer.Viewer' for any parameter
+    """
+    Implementation of a napari widget allowing to select a folder filled with images and 
+    segment them using cellpose.
+    """
     def __init__(self, napari_viewer):
         super().__init__()
         self.viewer = napari_viewer
@@ -41,13 +32,34 @@ class SerialWidget(QWidget):
         self.main_layout = QVBoxLayout()
         self.setLayout(self.main_layout)
 
-        self.main_layout.addWidget(QLabel("List of images"))
+        self.tabs = QTabWidget()
+        self.main_layout.addWidget(self.tabs)
+
+        # segmentation tab
+        self.segmentation = QWidget()
+        self._segmentation_layout = QVBoxLayout()
+        self.segmentation.setLayout(self._segmentation_layout)
+        self.tabs.addTab(self.segmentation, 'Segmentation')
+
+        # properties tab
+        self.properties = QWidget()
+        self._properties_layout = QVBoxLayout()
+        self.properties.setLayout(self._properties_layout)
+        self.tabs.addTab(self.properties, 'Properties')
+
+        # summary tab
+        self.summary = QWidget()
+        self._summary_layout = QVBoxLayout()
+        self.summary.setLayout(self._summary_layout)
+        self.tabs.addTab(self.summary, 'Summary')
+
+        self._segmentation_layout.addWidget(QLabel("List of images"))
 
         self.file_list = FolderList(napari_viewer)
-        self.main_layout.addWidget(self.file_list)
+        self._segmentation_layout.addWidget(self.file_list)
 
         self.folder_group = VHGroup('Folder selection')
-        self.main_layout.addWidget(self.folder_group.gbox)
+        self._segmentation_layout.addWidget(self.folder_group.gbox)
 
         self.btn_select_file_folder = QPushButton("Select data folder")
         self.folder_group.glayout.addWidget(self.btn_select_file_folder)
@@ -63,7 +75,7 @@ class SerialWidget(QWidget):
         self.folder_group.glayout.addWidget(self.btn_select_cellpose_model)
 
         self.run_group = VHGroup('Run analysis')
-        self.main_layout.addWidget(self.run_group.gbox)
+        self._segmentation_layout.addWidget(self.run_group.gbox)
 
         self.btn_run_on_current = QPushButton("Run on current image")
         self.run_group.glayout.addWidget(self.btn_run_on_current)
@@ -72,7 +84,7 @@ class SerialWidget(QWidget):
         self.run_group.glayout.addWidget(self.btn_run_on_folder)
 
         self.options_group = VHGroup('Options', orientation='G')
-        self.main_layout.addWidget(self.options_group.gbox)
+        self._segmentation_layout.addWidget(self.options_group.gbox)
 
         self.options_group.glayout.addWidget(QLabel("Batch size"), 0, 0, 1, 1)
         self.spinbox_batch_size = QSpinBox()
@@ -90,20 +102,26 @@ class SerialWidget(QWidget):
         self.spinbox_diameter.setValue(30)
         self.options_group.glayout.addWidget(self.spinbox_diameter, 2, 1, 1, 1)
 
-        self.add_connections()
-
         self.plot_group = VHGroup('Plots')
-        self.main_layout.addWidget(self.plot_group.gbox)
+        self._properties_layout.addWidget(self.plot_group.gbox)
 
-        self.sc = MplCanvas(self, width=5, height=4, dpi=100)
-        self.plot_group.glayout.addWidget(self.sc)
+        self.sc = MplCanvas(self, row=1, col=2, width=5, height=4, dpi=100)
         self.toolbar = NavigationToolbar(self.sc, self)
-
         self.plot_group.glayout.addWidget(self.toolbar)
         self.plot_group.glayout.addWidget(self.sc)
+
+        self.sc_sum = MplCanvas(self, row=1, col=2, width=5, height=4, dpi=100)
+        self.toolbar_sum = NavigationToolbar(self.sc_sum, self)
+        self._summary_layout.addWidget(self.toolbar_sum)
+        self._summary_layout.addWidget(self.sc_sum)
+        self.btn_load_summary = QPushButton("Load summary")
+        self._summary_layout.addWidget(self.btn_load_summary)
+
+        self.add_connections()
         
 
     def add_connections(self):
+        """Add callbacks"""
 
         self.btn_select_file_folder.clicked.connect(self._on_click_select_file_folder)
         self.btn_select_cellpose_model.clicked.connect(self._on_click_select_cellpose_model)
@@ -112,7 +130,7 @@ class SerialWidget(QWidget):
         self.btn_run_on_current.clicked.connect(self._on_click_run_on_current)
         self.btn_run_on_folder.clicked.connect(self._on_click_run_on_folder)
         self.qcbox_model_choice.currentTextChanged.connect(self._on_change_modeltype)
-    
+        self.btn_load_summary.clicked.connect(self._on_click_load_summary)
 
     def open_file(self):
         """Open file selected in list. Returns True if file was opened."""
@@ -135,7 +153,10 @@ class SerialWidget(QWidget):
             if mask_path.exists():
                 mask = skimage.io.imread(mask_path)
                 self.viewer.add_labels(mask, name='mask')
-                self.add_table_props()
+                props = load_props(self.output_folder, image_name)
+                self.add_table_props(props)
+
+        return True
 
     def _on_click_select_file_folder(self):
         """Interactively select folder to analyze"""
@@ -144,7 +165,7 @@ class SerialWidget(QWidget):
         self.file_list.update_from_path(self.file_folder)
 
     def _on_click_select_output_folder (self):
-        """Interactively select folder to analyze"""
+        """Interactively select folder where to save results"""
 
         self.output_folder = Path(str(QFileDialog.getExistingDirectory(self, "Select Directory")))
 
@@ -187,7 +208,8 @@ class SerialWidget(QWidget):
             diameter=diameter
         )
         self.viewer.add_labels(segmented, name='mask')
-        self.add_table_props()
+        props = load_props(self.output_folder, image_path)
+        self.add_table_props(props)
 
     def _on_click_run_on_folder(self):
         """Run cellpose on all images in folder"""
@@ -214,6 +236,19 @@ class SerialWidget(QWidget):
                 scaling_factor=1.0,
             )
 
+    def _on_click_load_summary(self):
+        """Load summary from folder"""
+
+        props = load_allprops(self.output_folder)
+        prop_names = ['eccentricity', 'feret_diameter_max']
+        for i in range(len(prop_names)):
+            self.sc_sum.ax[0,i].clear()
+            self.sc_sum.ax[0,i].hist(props[prop_names[i]], rwidth=0.85)
+            self.sc_sum.ax[0,i].figure.canvas.draw()
+            self.sc_sum.ax[0,i].tick_params(colors='white',labelsize=12)
+            self.sc_sum.ax[0,i].set_title(prop_names[i], fontsize=15, color='white')
+
+
     def _on_change_modeltype(self):
         "if selecting non-custom model, show diameter box"
 
@@ -224,26 +259,29 @@ class SerialWidget(QWidget):
             self.diameter_label.setVisible(False)
             self.spinbox_diameter.setVisible(False)
         
-    def add_table_props(self):
+    def add_table_props(self, props):
         """Add table with properties of segmented cells"""
 
-        props = regionprops_table(self.viewer.layers['mask'].data,
-                          properties=('label', 'area', 'eccentricity', 'solidity', 'feret_diameter_max'))
         self.viewer.layers['mask'].properties = props
         if self.props_table is None:
             self.props_table = TableWidget(layer=self.viewer.layers['mask'])
-            self.main_layout.addWidget(self.props_table)
+            self._properties_layout.addWidget(self.props_table)
         else:
             self.props_table._layer = self.viewer.layers['mask'] 
             self.props_table.update_content()
 
-        self.sc.axes.clear()       
-        self.sc.axes.hist(props['feret_diameter_max'])
-        self.sc.axes.figure.canvas.draw()
+        #self.sc.axes.clear()       
+        #self.sc.axes.hist(props['feret_diameter_max'], rwidth=0.85)
+        #self.sc.axes.figure.canvas.draw()
 
+        prop_names = ['eccentricity', 'feret_diameter_max']
+        for i in range(len(prop_names)):
+            self.sc.ax[0,i].clear()
+            self.sc.ax[0,i].hist(props[prop_names[i]], rwidth=0.85)
+            self.sc.ax[0,i].figure.canvas.draw()
+            self.sc.ax[0,i].tick_params(colors='white',labelsize=12)
+            self.sc.ax[0,i].set_title(prop_names[i], fontsize=15, color='white')
 
-
-    
 
 class VHGroup():
     """Group box with specific layout
@@ -267,3 +305,17 @@ class VHGroup():
             raise Exception(f"Unknown orientation {orientation}") 
 
         self.gbox.setLayout(self.glayout)
+
+class MplCanvas(FigureCanvasQTAgg):
+
+    def __init__(self, parent=None, col=1, row=1, width=5, height=4, dpi=100):
+        self.ax=np.array([[None]*col]*row)
+        fig = Figure(figsize=(width, height), dpi=dpi)
+        #self.axes = fig.add_subplot(111)
+        #fig, self.ax = plt.subplots(1, 3, figsize=(width, height), dpi=dpi)
+        count = 1
+        for i in range(row):
+            for j in range(col):
+                self.ax[i,j] = fig.add_subplot(row, col, count)
+                count+=1
+        super(MplCanvas, self).__init__(fig)
