3b8bc5686e485455031efe58b800c1a079321b84
guiwitz <guiwitz@users.noreply.github.com>
2022-07-20 23:04:41 +0200

    more tests

--- src/napari_serialcellpose/_tests/test_widget.py
+++ src/napari_serialcellpose/_tests/test_widget.py
@@ -2,6 +2,7 @@ from napari_serialcellpose import SerialWidget
 import numpy as np
 
 from pathlib import Path
+import shutil
 
 def test_load_single_image(make_napari_viewer):
     
@@ -43,36 +44,54 @@ def test_analyse_single_image_no_save(make_napari_viewer):
     assert viewer.layers[1].data.max() == 33
 
 
+def test_analyse_single_image_save(make_napari_viewer):
 
-
-# make_napari_viewer is a pytest fixture that returns a napari viewer object
-# capsys is a pytest fixture that captures stdout and stderr output streams
-'''def test_example_q_widget(make_napari_viewer, capsys):
-    # make viewer and add an image layer using our fixture
     viewer = make_napari_viewer()
-    viewer.add_image(np.random.random((100, 100)))
+    widget = SerialWidget(viewer)
 
-    # create our widget, passing in the viewer
-    my_widget = SerialWidget(viewer)
 
-    # call our widget method
-    my_widget._on_click()
+    mypath = Path('src/napari_serialcellpose/_tests/data/multifile/')
+    output_dir = Path('src/napari_serialcellpose/_tests/data/analyzed_multiple')
+    if output_dir.exists():
+        shutil.rmtree(output_dir)
+    output_dir.mkdir(exist_ok=True)
 
-    # read captured output and check that it's as we expected
-    captured = capsys.readouterr()
-    assert captured.out == "napari has 1 layers\n"
-    
-def test_example_magic_widget(make_napari_viewer, capsys):
+    widget.file_list.update_from_path(mypath)
+    widget.output_folder = output_dir
+    widget.file_list.setCurrentRow(0)
+
+    widget.qcbox_model_choice.setCurrentIndex(
+        [widget.qcbox_model_choice.itemText(i) for i in range(widget.qcbox_model_choice.count())].index('cyto2'))
+    widget.spinbox_diameter.setValue(70)
+    widget._on_click_run_on_current()
+
+    assert len(list(output_dir.glob('*mask.tif'))) == 1
+
+    widget._on_click_run_on_folder()
+    assert len(list(output_dir.glob('*mask.tif'))) == 4
+
+def test_mask_loading(make_napari_viewer):
+
     viewer = make_napari_viewer()
-    layer = viewer.add_image(np.random.random((100, 100)))
+    widget = SerialWidget(viewer)
 
-    # this time, our widget will be a MagicFactory or FunctionGui instance
-    my_widget = example_magic_widget()
+    mypath = Path('src/napari_serialcellpose/_tests/data/multifile/')
+    output_dir = Path('src/napari_serialcellpose/_tests/data/analyzed_multiple2')
+    if output_dir.exists():
+        shutil.rmtree(output_dir)
+    output_dir.mkdir(exist_ok=True)
 
-    # if we "call" this object, it'll execute our function
-    my_widget(viewer.layers[0])
+    widget.file_list.update_from_path(mypath)
+    widget.output_folder = output_dir
+    widget.file_list.setCurrentRow(0)
 
-    # read captured output and check that it's as we expected
-    captured = capsys.readouterr()
-    assert captured.out == f"you have selected {layer}\n"
-'''
+    widget.qcbox_model_choice.setCurrentIndex(
+        [widget.qcbox_model_choice.itemText(i) for i in range(widget.qcbox_model_choice.count())].index('cyto2'))
+    widget.spinbox_diameter.setValue(70)
+    widget._on_click_run_on_current()
+
+    widget.file_list.setCurrentRow(1)
+    assert len(viewer.layers) == 1
+
+    widget.file_list.setCurrentRow(0)
+    assert len(viewer.layers) == 2
