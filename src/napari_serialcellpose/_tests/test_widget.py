0a01cb24ba8ad030e0ad2745b7eea4b0d82d59bd
guiwitz <guiwitz@users.noreply.github.com>
2022-07-26 17:36:33 +0200

    test that properties are measured

--- src/napari_serialcellpose/_tests/test_widget.py
+++ src/napari_serialcellpose/_tests/test_widget.py
@@ -1,5 +1,6 @@
 from napari_serialcellpose import SerialWidget
 import numpy as np
+import pandas as pd
 
 from pathlib import Path
 import shutil
@@ -43,8 +44,7 @@ def test_analyse_single_image_no_save(make_napari_viewer):
     assert viewer.layers[1].name == 'mask'
     assert viewer.layers[1].data.max() == 33
 
-
-def test_analyse_single_image_save(make_napari_viewer):
+def test_analyse_multi_image(make_napari_viewer):
 
     viewer = make_napari_viewer()
     widget = SerialWidget(viewer)
@@ -70,6 +70,39 @@ def test_analyse_single_image_save(make_napari_viewer):
     widget._on_click_run_on_folder()
     assert len(list(output_dir.glob('*mask.tif'))) == 4
 
+def test_analyse_multi_image_props(make_napari_viewer):
+
+    viewer = make_napari_viewer()
+    widget = SerialWidget(viewer)
+
+    mypath = Path('src/napari_serialcellpose/_tests/data/multifile/')
+    output_dir = Path('src/napari_serialcellpose/_tests/data/analyzed_multiple3')
+    if output_dir.exists():
+        shutil.rmtree(output_dir)
+    output_dir.mkdir(exist_ok=True)
+
+    widget.file_list.update_from_path(mypath)
+    widget.output_folder = output_dir
+    widget.file_list.setCurrentRow(0)
+
+    widget.qcbox_model_choice.setCurrentIndex(
+        [widget.qcbox_model_choice.itemText(i) for i in range(widget.qcbox_model_choice.count())].index('cyto2'))
+    widget.spinbox_diameter.setValue(70)
+    widget.check_props['size'].setChecked(True)
+    widget.check_props['intensity'].setChecked(True)
+    widget.qcbox_channel_analysis.setCurrentIndex(2)
+
+    widget._on_click_run_on_folder()
+    assert len(list(output_dir.glob('*mask.tif'))) == 4
+
+    # check that the properties are correct
+    df = pd.read_csv(output_dir.joinpath(
+        'tables',
+        Path(widget.file_list.currentItem().text()).stem + '_props.csv'
+    )
+    )
+    assert df.shape[1] == 9
+
 def test_mask_loading(make_napari_viewer):
 
     viewer = make_napari_viewer()
@@ -90,8 +123,10 @@ def test_mask_loading(make_napari_viewer):
     widget.spinbox_diameter.setValue(70)
     widget._on_click_run_on_current()
 
+    # check that when selecting the second file, we get only 2 channels and no mask
     widget.file_list.setCurrentRow(1)
-    assert len(viewer.layers) == 1
-
-    widget.file_list.setCurrentRow(0)
     assert len(viewer.layers) == 2
+
+    # check that when selecting the first file, we get  2 channels and a mask
+    widget.file_list.setCurrentRow(0)
+    assert len(viewer.layers) == 3
