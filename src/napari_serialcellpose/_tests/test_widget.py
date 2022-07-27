4dd7c37fbf2175fd8e062cc269225cdb8034d082
guiwitz <guiwitz@users.noreply.github.com>
2022-07-27 18:23:10 +0200

    add test for single file

--- src/napari_serialcellpose/_tests/test_widget.py
+++ src/napari_serialcellpose/_tests/test_widget.py
@@ -44,6 +44,44 @@ def test_analyse_single_image_no_save(make_napari_viewer):
     assert viewer.layers[1].name == 'mask'
     assert viewer.layers[1].data.max() == 33
 
+def test_analyse_single_image_save(make_napari_viewer):
+    
+    viewer = make_napari_viewer()
+    widget = SerialWidget(viewer)
+
+    mypath = Path('src/napari_serialcellpose/_tests/data/single_file_multichannel')
+              
+    output_dir = Path('src/napari_serialcellpose/_tests/data/analyzed_single')
+    if output_dir.exists():
+        shutil.rmtree(output_dir)
+    output_dir.mkdir(exist_ok=True)
+
+    widget.file_list.update_from_path(mypath)
+    widget.output_folder = output_dir
+    widget.file_list.setCurrentRow(0)
+    # Check that selecting cyto2 displays diameter choice
+    assert widget.spinbox_diameter.isVisible() is False
+    widget.qcbox_model_choice.setCurrentIndex(
+        [widget.qcbox_model_choice.itemText(i) for i in range(widget.qcbox_model_choice.count())].index('cyto2')
+    )
+    # not working for some reason
+    #assert widget.spinbox_diameter.isVisible() is True
+    
+    # set diameter and run segmentation
+    widget.spinbox_diameter.setValue(60)
+    widget.qcbox_channel_to_segment.setCurrentIndex(1)
+    widget.qcbox_channel_helper.setCurrentIndex(2)
+    widget.check_clear_border.setChecked(False)
+
+    widget.check_props['size'].setChecked(True)
+    widget.check_props['intensity'].setChecked(True)
+    widget.qcbox_channel_analysis.setCurrentIndex(2)
+
+    widget._on_click_run_on_current()
+
+    assert len(list(output_dir.glob('*mask.tif'))) == 1
+    assert len(list(output_dir.joinpath('tables').glob('*_props.csv'))) == 1
+
 def test_analyse_multi_image(make_napari_viewer):
 
     viewer = make_napari_viewer()
